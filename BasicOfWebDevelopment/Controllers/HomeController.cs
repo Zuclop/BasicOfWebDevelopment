0318d61decf7e161719c73ceac899ea323d30292
Yevhen <Yevhen.boiko@gmail.com>
2016-10-24 21:15:28 +0300

    Commit

---
+++ BasicOfWebDevelopment/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BasicOfWebDevelopment.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [AcceptVerbs("GET", "POST", "PUT", "DELETE")]
+        public string DoSomething()
+        {
+            if(FileWriter.getInstance().WriteInformation(HttpContext.Request.UserHostAddress, HttpContext.Request.HttpMethod, HttpContext.Request.Url.AbsolutePath))
+            {
+                return HttpContext.Request.HttpMethod;
+            }
+            else
+                return "Error";
+        }
+    }
+}
---
+++ BasicOfWebDevelopment/FileWriter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace BasicOfWebDevelopment
+{
+    public class FileWriter
+    {
+        private static FileWriter instance;
+        private static object threadLock = new object();
+        private static string path = @"log.txt";
+        private FileWriter()
+        { }
+
+        public static FileWriter getInstance()
+        {
+            if (instance == null)
+            { 
+                instance = new FileWriter();
+                if (!File.Exists(path))
+                {
+                    File.Create(path);
+                }
+            }
+            return instance;
+        }
+
+        public bool WriteInformation(string ip, string method, string url)
+        {
+            lock (threadLock)
+            {
+                try
+                {
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(path, true))
+                    {
+                        file.WriteLine("Ip: " + ip);
+                        file.WriteLine("Method: " + method);
+                        file.WriteLine("Url: " + url);
+                        file.WriteLine("");
+                    }
+                    return true;
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.StackTrace);
+                    return false;
+                }
+            }
+        }
+    }
+}
